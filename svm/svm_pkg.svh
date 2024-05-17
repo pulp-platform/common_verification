54d2731f19d223ff8aaaa1f66543f108a086e3f6
fhaque60 <faisal.haque@impare.cloud>
2024-05-17 05:27:27 +0000

    first check in

--- svm/svm_pkg.svh
+++ svm/svm_pkg.svh
@@ -14,7 +14,9 @@
 //
 package svm_pkg;
   virtual class svm_component;
-    string  name;
+    string       name;
+    svm_logger   log;
+
     virtual task reset ();
     virtual task build ();
     virtual task connect ();
@@ -23,7 +25,8 @@ package svm_pkg;
     virtual task finish();
   endclass
   virtual class svm_trans;
-    string name;
+    string       name;
+    svm_logger   log;
 
     virtual function svm_trans cp();
     virtual task pr();
@@ -62,7 +65,7 @@ package svm_pkg;
   //TODO: create the build task to construt each component
   //TODO: Create configure task to use config object to configure components
   //TODO: Create run task to invoke the run tasks of each component.  Each
-  //component should fork off the run thread fom within its run task
+  
   //TODO: How are you going to determine when to exit?
   //TODO: Create a reset task to call the reset task of each component
   //TODO: Call finish task to to call $finish when all components are done
@@ -71,9 +74,53 @@ package svm_pkg;
   //received a response
   //b. Each component takes a key from a semaphore and puts it back when its
   //done
+
+    /***************************************/
+    /**This task is optional do we need it? */
+    /***************************************/
     virtual task run_test (svm_test test_to_run);
     //TODO: This is optional  Do we need a test class ro should we use plus
     //args to control the simulation
+  endclass
+
+  //this monitors the response and sends it to the scoreboard
+  class svm_monitor extends svm_component;
+    svm_trans trans;
+    mailbox   svm_trans mon2scbd;
+
+    virtual task sample ();
+    //TODO: Add a new() function to construct this object
+    //TODO: Create a run() task which forks off sample()
+    //TODO: Create sample() task in which waits on a valid signal and then
+    //sample the rest of the signals and send them to mbailbox. It needs to run
+    //in a while (1) loop
+  endclass
+  class svm_scoreboard  extends svm_component;
+    svm_trans         req;
+    svm_trans         rsp;
+    mailbox svm_trans drvr2scbd;
+    mailbox svm_trans mon2scbd;
+
+    svm_predictor     predictor;
+    virtual function  bit check_rsp();
+    virtual task          rcv_from_drvr();
+    virtual task          rcv_from_mon ();
+  endclass
+
+  class svm_predictor extends svm_component;
+    svm_trans trans_in;
+    exp_rspQ svm_trans [$];
+
+    virtual task set trans (svm_trans trans_in);
+      this.trans_in = trans_in;
+      process_trans();
+    endtask
+
+    virtual task process_trans ();
+      
+    virtual function svm_trans get_next_exp_rsp ();
+      get_next_exp_rsp = exp_rspQ.pop_front();
+    endfunction
   endclass
 endpackage	
 
